c8a93c2c9e9a2ef097338ad1910b740b81cacf5f
Daniel Eisen <eisen.dan@gmail.com>
2017-12-26 19:48:24 -0800

    adds repeatable field for articles in allard post types

--- extend/allard-post-types/allard-custom-fields.php
+++ extend/allard-post-types/allard-custom-fields.php
@@ -61,16 +61,53 @@ function allard_register_metabox() {
 		'type' => 'text_small',
 	) );
 	
+//	repeatable group article
+	
+	$article_field_id = $cmb_allard->add_field( array(
+	'id'          => 'Articles',
+	'type'        => 'group',
+	'name'		  => 'Articles In This Issue',
+	'description' => __( 'Add Article Entries Below.', 'cmb2' ),
+	'options'     => array(
+		'group_title'   => __( 'Article {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+		'add_button'    => __( 'Add Another Article To This Issue', 'cmb2' ),
+		'remove_button' => __( 'Remove This Article', 'cmb2' ),
+		'sortable'      => true, // beta
+		// 'closed'     => true, // true to have the groups closed by default
+	),
+) );
 
 
-	$cmb_allard->add_field( array(
-		'name'    => esc_html__( 'Synopsis - Articles, Essay Contest Winner, Book Review', 'cmb2' ),
-		'desc'    => esc_html__( 'Synopsis of the journal issue without any abstracts', 'cmb2' ),
-		'id'      => $prefix . 'synopsis',
-		'type'    => 'wysiwyg',
-		'options' => array(
-			'textarea_rows' => 25,
-		),
-	) );
+$cmb_allard->add_group_field( $article_field_id, array(
+	'name' => 'Title',
+	'id'   => 'title',
+	'type' => 'text',
+//	'repeatable' => true,
+) );
+
+$abstract_wysig = array(
+        'wpautop' => true, // use wpautop?
+        'media_buttons' => false, // show insert/upload button(s)
+        'textarea_name' => $editor_id, // set the textarea name to something different, square brackets [] can be used here
+        'textarea_rows' => get_option('default_post_edit_rows', 10), // rows="..."
+        'tabindex' => '',
+        'editor_css' => '', // intended for extra styles for both visual and HTML editors buttons, needs to include the `<style>` tags, can use "scoped".
+        'editor_class' => '', // add extra class(es) to the editor textarea
+        'teeny' => true, // output the minimal editor config used in Press This
+        'dfw' => false, // replace the default fullscreen with DFW (needs specific css)
+        'tinymce' => true, // load TinyMCE, can be used to pass settings directly to TinyMCE using an array()
+        'quicktags' => true // load Quicktags, can be used to pass settings directly to Quicktags using an array()
+    );
+	
+$cmb_allard->add_group_field( $article_field_id, array(
+	'name' => 'Abstract',
+	'id'   => 'abstract',
+	'type'    => 'wysiwyg',
+    'options' => $abstract_wysig,
+) );
+	
+
+
+
 
 }
